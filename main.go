ebd23705a8abb44e0a9a7127677ef7fccab9ad61
Kacper Bąk <kacper.bak@sonderfin.com>
2021-09-02 18:23:12 +0200

    refactor: structure of project

--- main.go
+++ main.go
@@ -17,19 +17,6 @@ type Post struct {
 
 var posts []Post
 
-func main() {
-
-	router := mux.NewRouter()
-
-	router.HandleFunc("/posts", getPosts).Methods("GET")
-	router.HandleFunc("/posts", createPost).Methods("POST")
-	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
-	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
-	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
-
-	http.ListenAndServe(":8000", router)
-}
-
 func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(posts)
@@ -86,4 +73,20 @@ func deletePost(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode(books)
+}
+
+func main() {
+
+	router := mux.NewRouter()
+
+	posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
+	posts = append(posts, Post{ID: "2", Title: "My second post", Body: "This is the content of my second post"})
+
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+
+	http.ListenAndServe(":8000", router)
 }
