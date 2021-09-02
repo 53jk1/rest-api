1aee3782852b9a8066ee1a77a5a9c96009c6c5bf
Kacper Bąk <kacper.bak@sonderfin.com>
2021-09-02 18:21:47 +0200

    feat: deletePost function

--- main.go
+++ main.go
@@ -77,5 +77,13 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func deletePost(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
