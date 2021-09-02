719dda5434c33f42c674c05cfce6be9a7769fbe9
Kacper Bąk <kacper.bak@sonderfin.com>
2021-09-02 18:46:08 +0200

    feat: getPosts function working

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -16,65 +14,64 @@ type Post struct {
 }
 
 var posts []Post
-var books interface{}
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(posts)
 }
 
-func createPost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var post Post
-	_ = json.NewDecoder(r.Body).Decode(post)
-	post.ID = strconv.Itoa(rand.Intn(1000000))
-	posts = append(posts, post)
-	json.NewEncoder(w).Encode(&post)
-}
+// func createPost(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	var post Post
+// 	_ = json.NewDecoder(r.Body).Decode(post)
+// 	post.ID = strconv.Itoa(rand.Intn(1000000))
+// 	posts = append(posts, post)
+// 	json.NewEncoder(w).Encode(&post)
+// }
 
-func getPost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, item := range posts {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			break
-		}
-		return
-	}
-	json.NewEncoder(w).Encode(&Post{})
-}
+// func getPost(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	for _, item := range posts {
+// 		if item.ID == params["id"] {
+// 			json.NewEncoder(w).Encode(item)
+// 			break
+// 		}
+// 		return
+// 	}
+// 	json.NewEncoder(w).Encode(&Post{})
+// }
 
-func updatePost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range posts {
-		if item.ID == params["id"] {
-			posts = append(posts[:index], posts[index+1:]...)
+// func updatePost(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	for index, item := range posts {
+// 		if item.ID == params["id"] {
+// 			posts = append(posts[:index], posts[index+1:]...)
 
-			var post Post
-			_ = json.NewDecoder(r.Body).Decode(post)
-			post.ID = params["id"]
-			posts = append(posts, post)
-			json.NewEncoder(w).Encode(&post)
+// 			var post Post
+// 			_ = json.NewDecoder(r.Body).Decode(post)
+// 			post.ID = params["id"]
+// 			posts = append(posts, post)
+// 			json.NewEncoder(w).Encode(&post)
 
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(posts)
-}
+// 			return
+// 		}
+// 	}
+// 	json.NewEncoder(w).Encode(posts)
+// }
 
-func deletePost(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range posts {
-		if item.ID == params["id"] {
-			posts = append(posts[:index], posts[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
-}
+// func deletePost(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	for index, item := range posts {
+// 		if item.ID == params["id"] {
+// 			posts = append(posts[:index], posts[index+1:]...)
+// 			break
+// 		}
+// 	}
+// 	json.NewEncoder(w).Encode(books)
+// }
 
 func main() {
 
@@ -84,10 +81,10 @@ func main() {
 	posts = append(posts, Post{ID: "2", Title: "My second post", Body: "This is the content of my second post"})
 
 	router.HandleFunc("/posts", getPosts).Methods("GET")
-	router.HandleFunc("/posts", createPost).Methods("POST")
-	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
-	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
-	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	// router.HandleFunc("/posts", createPost).Methods("POST")
+	// router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	// router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	// router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
 
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":8082", router)
 }
