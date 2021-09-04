b4f063e94761fb8d4abe78345fd41a786011f563
53jk1 <bonczalke@gmail.com>
2021-09-04 11:04:19 +0200

    feat: GetPosts function SQL implementation

--- main.go
+++ main.go
@@ -23,6 +23,20 @@ var err error
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var posts []Post
+	result, err := db.Query("SELECT id, title from posts")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+		var post Post
+		err := result.Scan(&post.ID, &post.Title)
+		if err != nil {
+			panic(err.Error())
+		}
+		posts = append(posts, post)
+	}
 	json.NewEncoder(w).Encode(posts)
 }
 
